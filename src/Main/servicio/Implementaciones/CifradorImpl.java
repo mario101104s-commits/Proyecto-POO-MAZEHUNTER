5d7b7c94a180ac81d63110ba38d8be42e443ad6b
Jose Berroteran <joseale2005b@gmail.com>
2025-11-12 20:31:23 -0400

    Ya la clase Cifrado y su interfaz están funcionables.

--- src/Main/servicio/Implementaciones/CifradorImpl.java
+++ src/Main/servicio/Implementaciones/CifradorImpl.java
@@ -1,4 +1,39 @@
 package Main.servicio.Implementaciones;
 
-public class CifradorImpl {
+import Main.servicio.Interfaces.Cifrador;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class CifradorImpl implements Cifrador {
+
+    public CifradorImpl() {
+    }
+
+    private static final String CLAVE_CIFRADO = "MiClaveSecreta12";
+    public String cifrarContrasenia(String contrasenia) {
+        try {
+            SecretKeySpec clave = new SecretKeySpec(CLAVE_CIFRADO.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, clave);
+            byte[] contraseniaCifrada = cipher.doFinal(contrasenia.getBytes());
+            return Base64.getEncoder().encodeToString(contraseniaCifrada);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public String descifrarContrasenia(String contraseniaCifrada) {
+        try {
+            SecretKeySpec clave = new SecretKeySpec(CLAVE_CIFRADO.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, clave);
+            byte[] contraseniaBytes = Base64.getDecoder().decode(contraseniaCifrada);
+            byte[] contraseniaDescifrada = cipher.doFinal(contraseniaBytes);
+            return new String(contraseniaDescifrada);
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
--- src/Main/servicio/Interfaces/Cifrador.java
+++ src/Main/servicio/Interfaces/Cifrador.java
@@ -1,4 +1,6 @@
 package Main.servicio.Interfaces;
 
 public interface Cifrador {
+    public String cifrarContrasenia(String contrasenia);
+    public String descifrarContrasenia(String contraseniaCifrada);
 }
